5fb9fe19d8abe9e1db6fdca0c28bcd6190d2787c
salrio <salrio74@gmail.com>
2020-09-16 14:20:06 +0200

    incluido opcion ver post por año

--- pages/year-archive/_slug.vue
+++ pages/year-archive/_slug.vue
@@ -11,16 +11,35 @@
       <h1 class="text-gray-600 border-b border-gray-300 my-5 font-bold">
         Post por año
       </h1>
-      <ul class="flex flex-wrap">
-        <li
+      <div
+        v-for="year of years"
+        :key="year.year"
+        :set="(tempYear = getYear(year.year))"
+      >
+        <h2>{{ tempYear }}</h2>
+
+        <ul
           v-for="article of articles"
           :key="article.slug"
-          class="xs:w-full md:w-full px-2 xs:mb-6 md:mb-12 article-card"
+          class="flex flex-wrap"
+          :set="(articleYear = getYear(article.year))"
         >
-          <div>{{ article.title }}</div>
-          <div>{{ article.year }}</div>
-        </li>
-      </ul>
+          <li
+            v-if="articleYear == tempYear"
+            class="xs:w-full md:w-full px-2 xs:mb-6 md:mb-12 article-card"
+          >
+            <NuxtLink
+              :to="{ name: 'blog-slug', params: { slug: article.slug } }"
+              class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
+            >
+              <div>
+                {{ article.title }}
+              </div>
+              <div>{{ formatDate(article.year) }}</div>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
     </div>
 
     <Footer />
@@ -46,13 +65,43 @@ export default {
         console.error(err);
       });
 
+    const years = await $content('articles', params.year)
+      .only(['year'])
+      .without(['path', 'extension'])
+      .sortBy('year', 'asc')
+      .fetch()
+      .catch(err => {
+        error({ statusCode: 404, message: 'Page not found' });
+        console.error(err);
+      });
+
+    console.log(years);
+
     return {
       articles,
+      years,
     };
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return new Date(date).toLocaleDateString('es', options);
+    },
+    getYear(date) {
+      const options = { year: 'numeric' };
+      return new Date(date).toLocaleDateString('es', options);
+    },
   },
 };
 </script>
 
 <style lang="scss">
 @import '~/assets/css/_colors.scss';
+h2 {
+  margin: 1.414em 0 0;
+  padding-bottom: 0.5em;
+  font-size: 1em;
+  color: #9ba1a6;
+  border-bottom: 1px solid #f2f3f3;
+}
 </style>
