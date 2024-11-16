c5b101a6c4b7584775e5e76ba02da4604e873ef7
Salvador Rioboo Naranjo <salrio74@gmail.com>
2024-11-16 14:40:14 +0100

    feat(css): migrated Footer.vue to tailwindcss

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -1,14 +1,16 @@
 <template>
   <footer>
-    <div class="footer__info">
-      <div class="container footer__info-container">
-        <div class="footer__copyright">
+    <div
+      class="bg-gray-200 py-6 px-4 h-14 border-t border-solid border-t-gray-400"
+    >
+      <div class="flex justify-between w-full flex-col sm:flex-row">
+        <div class="text-gray-500 text-center">
           © {{ year }}
           <span>
             Salvador. Powered by <a href="https://www.nuxtjs.org">Nuxt</a>
           </span>
         </div>
-        <a href="#top" class="footer__top-page">^</a>
+        <a href="#top" class="block text-center">^</a>
       </div>
     </div>
     <!-- CookieAlert  -->
@@ -23,52 +25,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-@import '~/assets/css/_base';
-
-.footer {
-  &__info {
-    background-color: $grey-light; //$background-secondary;
-    padding: 1.5em 1em;
-    border-top: 1px solid $grey-medium;
-    height: $he-footer;
-  }
-
-  &__info-container {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    flex-direction: column;
-
-    @include sm {
-      flex-direction: row;
-    }
-  }
-
-  &__copyright {
-    text-align: center;
-    color: $grey;
-    @include sm {
-      text-align: inherit;
-    }
-    & {
-      color: $grey-dark;
-    }
-  }
-
-  &__top-page {
-    color: $grey;
-    display: block;
-    text-align: center;
-    line-height: 1;
-    -webkit-transition: 1s;
-    -moz-transition: 1s;
-    transition: 1s;
-
-    @include sm {
-      text-align: right;
-    }
-  }
-}
-</style>
