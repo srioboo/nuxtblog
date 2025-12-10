a9a4f9f849618843362b0337ec119e3b98f1c5a5
Salvador Rioboo Naranjo <salrio74@gmail.com>
2025-12-10 20:58:49 +0100

    fix: some review changes

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -1,12 +1,12 @@
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
     // handle error, e.g. report to a service
-    console.log("Se ha producido el error: " + error + "\n" + instance + "\n" + info);
+    console.log("Se ha producido el error:", { error, instance, info });
   }
 
   // Also possible
   nuxtApp.hook('vue:error', (error, instance, info) => {
     // handle error, e.g. report to a service
-    console.log("Se ha producido el error: " + error + "\n" + instance + "\n" + info);
+    console.log("Se ha producido el error:", { error, instance, info });
   })
 })
